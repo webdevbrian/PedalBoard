312f48ebbd8205587b1b035f6eade8a1a873b2c1
Brian Kinney <webdevbrian@gmail.com>
2025-10-21 18:02:30 -0400

    Old file removable cleanup to lessen confusion

--- scripts/deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#usage : scripts/deps.sh
-
-lib/tartJS/tools/goog/build/depswriter.py \
---root_with_prefix='src/ ../../../../../src/' \
---root_with_prefix='lib/ ../../../../../lib/' \
---output_file='example/deps.js'
